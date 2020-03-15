a1d56557c61ded6f11c03c4b11f0a83832f995d7
DANY GOMEZe <arleygomez2009@hotmail.com>
2020-03-15 16:38:48 -0500

    ijercio4

---
+++ includes/db.php
@@ -0,0 +1,43 @@
+<?php
+//Datos de conexion
+$host="localhost";//ip, maquina
+$user="root";
+$password="12345678";
+$base="personas";
+
+
+class DB{
+
+public static function init (){
+
+
+
+echo "iniciando base datos";
+
+
+
+public static function query(){
+
+    // crear la conexion
+    $ 
+    }
+
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
